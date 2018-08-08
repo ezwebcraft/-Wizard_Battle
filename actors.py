834952dccc50a27f849b142bc7547a9e97a5b307
Elvin Ramirez <real8686@gmail.com>
2018-08-07 23:46:07 -0400

    add dragon class with fire mod

--- actors.py
+++ actors.py
@@ -39,4 +39,20 @@ class Wizard(Creatures):
 class SmallAnimal(Creatures):
     def get_defensive_roll(self):
         base_roll = super().get_defensive_roll()
-        return base_roll/2
+        return base_roll / 2
+
+
+class Dragon(Creatures):
+    def __init__(self, name, level, scaliness, breath_fire):
+        super().__init__(name, level)
+        self.breath_fire = breath_fire
+        self.scaliness = scaliness
+
+    def get_defensive_roll(self):
+        base_roll = super().get_defensive_roll()
+        fire_modifier = None
+
+        if self.breath_fire:
+            fire_modifier = 5
+        else:
+            fire_modifier = 1
